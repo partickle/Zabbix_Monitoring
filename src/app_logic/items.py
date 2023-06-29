c091b69de3578b664b879ce2ecd6203f4a82276b
bladway <113307409+bladway@users.noreply.github.com>
2023-06-29 16:07:33 +0300

    Revert "Developing of the main hosts window"

--- src/app_logic/items.py
+++
@@ -1,9 +0,0 @@
-class Items:
-    def __init__(self, zabbix):
-        self.zabbix = zabbix
-    
-
-    def get_items(self, host):
-        items = self.zabbix.item.get(hostids=host['hostid'], output=['itemid','name','key_'])
-        return items
-   
