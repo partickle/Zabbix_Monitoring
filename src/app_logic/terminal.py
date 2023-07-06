3db6f6a7dc0b7d68c4ea5bce825db17893a9bc16
Persifal <md.putilin@yandex.ru>
2023-07-06 04:23:24 +0300

    One more refactoring (modules), fill new css-files

--- src/app_logic/terminal.py
+++ src/app_logic/terminal.py
@@ -8,7 +8,8 @@ import datetime
 class Terminal:
     def __init__(self, zabbix):
         self.zabbix = zabbix
-        self.last_checked_str = ""  # Создаем переменную с последним проверенным логом
+        # Создаем переменную с последним проверенным логом
+        self.last_checked_str = ""
         self.last_checked_str_arr = []  # И с множеством логов
 
     # Метод для проверки новых логов
@@ -16,23 +17,28 @@ class Terminal:
         new_checked_str = []  # Создаем массив для хранения новых логов
 
         # Делаем заброс event.get
-        logs = self.zabbix.event.get(output='extend', sortfield='clock', sortorder='DESC')
+        logs = self.zabbix.event.get(
+            output='extend', sortfield='clock', sortorder='DESC'
+        )
 
         # Если логов нет, то возвращаем False (обновлений нет)
         if len(logs) == 0:
             return False
 
         # Прогоняем логи через цикл
-        for log in logs:  # Не reversed, потому что они и так уже в обратном порядке
+        for log in logs:  # Не reversed, потому что они уже в обратном порядке
             # Заводим переменную полученной строки сразу с парсингом
-            new_str = log['eventid'] + "." + get_norm_data(int(log['clock'])) + " " + log['name']
-            if self.last_checked_str == new_str:  # Если это последняя строка в терминале, то...
+            new_str = log['eventid'] + "."\
+                      + get_norm_data(int(log['clock'])) + " " + log['name']
+            # Если это последняя строка в терминале, то...
+            if self.last_checked_str == new_str:
                 break  # Прерываем цикл
-            new_checked_str.append(new_str)  # Если нет, то добавляем ее в массив
+            new_checked_str.append(new_str)  # Если нет - добавляем ее в массив
 
         # Если в массиве есть обновления, то...
         if len(new_checked_str) != 0:
-            self.last_checked_str_arr = list(reversed(new_checked_str))  # Сохраняем их
+            # Сохраняем их
+            self.last_checked_str_arr = list(reversed(new_checked_str))
             self.last_checked_str = new_checked_str[0]
             return True  # И отправляем сигнал того, что пришло обновление
 
@@ -41,17 +47,26 @@ class Terminal:
     # Метод для заполнения существующих логов
     def log_full_request(self, label):
         # Сообщение о формате отображения логов
-        label.setText("FORMAT_LOGS: \"EventId.yyyy-mm-dd. HH:HM:SS Message\"\n===================\n\n")
+        label.setText(
+            "FORMAT_LOGS: \"EventId.yyyy-mm-dd. "
+            "HH:HM:SS Message\"\n===================\n\n"
+        )
 
-        logs = self.zabbix.event.get(output='extend', sortfield='clock', sortorder='DESC')
+        logs = self.zabbix.event.get(
+            output='extend', sortfield='clock', sortorder='DESC'
+        )
         if len(logs) == 0:
             return
 
         # Прогоняем все существующие логи на данный момент
-        for log in reversed(logs):  # Логи обычно записываются в обратном порядке, поэтому reversed
-            new_str = log['eventid'] + "." + get_norm_data(int(log['clock'])) + " " + log['name']
-            label.setText(label.text() + new_str + "\n")  # Добавляем в лейбл лог
-            self.last_checked_str = new_str # И обновляем последнюю добавленную строку
+        # Логи обычно записываются в обратном порядке, поэтому reversed
+        for log in reversed(logs):
+            new_str = log['eventid'] + "."\
+                      + get_norm_data(int(log['clock'])) + " " + log['name']
+            # Добавляем в лейбл лог
+            label.setText(label.text() + new_str + "\n")
+            # И обновляем последнюю добавленную строку
+            self.last_checked_str = new_str
 
 
 # Функция для преобразования даты из UNIX timestamp в привычную
